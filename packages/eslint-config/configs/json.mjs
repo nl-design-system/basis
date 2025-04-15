58636138c7b3e3f87533b8bd3e515d93d694a47c
matijs <matijs@gmail.com>
2025-04-15 13:33:54 +0200

    feat: add json

---
+++ packages/eslint-config/configs/json.mjs
@@ -0,0 +1,12 @@
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    extends: ['json/recommended'],
+    files: ['**/*.json'],
+    ignores: ['package-lock.json'],
+    language: 'json/json',
+    plugin: { json },
+  },
+]);
